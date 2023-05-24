2dc8bc9f5f8825064ff609ba9c9e84a972c5af3e
Jeffrey Morgan <107521911+MADDOG25@users.noreply.github.com>
2023-05-23 22:24:22 -0500

    prueba de product landing page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.scroll-navbar', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.scroll-section2', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
